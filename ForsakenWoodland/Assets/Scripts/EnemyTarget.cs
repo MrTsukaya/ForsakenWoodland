419b5e7c1d5beabfad462b10505d4219a175d3ab
LordPapkin <papkin2013@gmail.com>
2022-01-30 00:08:49 +0100

    Revert "Pathfinding Update"
    This reverts commit 3a6cde19fac71a54b5ee55a0c69cbf2d065f32d9.

--- ForsakenWoodland/Assets/Scripts/EnemyTarget.cs
+++ ForsakenWoodland/Assets/Scripts/EnemyTarget.cs
@@ -5,41 +5,20 @@ using UnityEngine.AI;
 
 public class EnemyTarget : MonoBehaviour
 {
-    Transform target;    
+    Transform target;
     NavMeshAgent agent;
-
-    public float speed = 1f;
-
-    private BoxCollider2D triggerZone;
-    public Vector3 startingPoint;   
-    
-
     // Start is called before the first frame update
     void Start()
-    {        
+    {
         target = PlayerController.pm.player.transform;
-        startingPoint = transform.position;
-        triggerZone = GetComponent<BoxCollider2D>();
-
-
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
-        agent.speed = speed;
     }
-    
-    private void OnTriggerEnter2D(Collider2D collision)
+
+    // Update is called once per frame
+    void Update()
     {
-        if (collision.CompareTag("Player"))
-        {
-            agent.SetDestination(target.position);
-        }
-    }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            agent.SetDestination(startingPoint);
-        }
+        agent.SetDestination(target.position);
     }
 }
